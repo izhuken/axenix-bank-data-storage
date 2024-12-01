7b75d994a62211dbf328ea93cff3755f40193fed
Александр Кизименко <99537607+izhuken@users.noreply.github.com>
2024-12-01 06:25:45 +0300

    Update create_report.py

--- src/backend/external_api/api/routers/create_report.py
+++ src/backend/external_api/api/routers/create_report.py
@@ -17,14 +17,14 @@ external_report_service = ExternalReportService()
 @create_report_router.post("/create_report", status_code=201, response_model=CreateReportResponse)
 async def create_report(data: CreateReportRequest, request: Request) -> CreateReportResponse:
     jwt_token = request.headers.get("authorization").split(" ")[1]
-    user_id = jwt.decode(jwt_token, algorithms=ALGORITHM, key=SECRET_KEY)["user_id"]
+    user_id = jwt.decode(jwt_token, algorithms=ALGORITHM, key=SECRET_KEY)
 
     data_dict = data.model_dump()
 
     builder_report_service = BuilderReportService(data_dict["report_name"], data_dict["values"], data_dict["format_report"], request)
     path_file = await builder_report_service.create_file()
 
-    result = await external_report_service.create(data=CreateReportInsert(**{"title": data.report_name, "url": path_file, "user_id": user_id}))
+    result = await external_report_service.create(data=CreateReportInsert(**{"title": data.report_name, "url": path_file, "user_id": user_id.get("sub")}))
     if isinstance(result, ErrorResponse):
         raise HTTPException(status_code=result.status_code, detail=result.detail)
     return result["url"]
